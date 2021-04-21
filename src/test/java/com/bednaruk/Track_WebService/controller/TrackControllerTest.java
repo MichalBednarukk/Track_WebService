42150b149387b574c4d2ef40ca5d35278f0b4924
Michał <mbednaruk12@gmail.com>
2021-04-21 15:15:48 +0200

    UpdateTrack test

--- src/test/java/com/bednaruk/Track_WebService/controller/TrackControllerTest.java
+++ src/test/java/com/bednaruk/Track_WebService/controller/TrackControllerTest.java
@@ -3,18 +3,22 @@ package com.bednaruk.Track_WebService.controller;
 import com.bednaruk.Track_WebService.entity.TrackApp;
 import com.bednaruk.Track_WebService.repository.TrackRepo;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
 
 import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -51,6 +55,36 @@ class TrackControllerTest {
         assertThat(trackApp.getBody()).isEqualTo("body");
         assertThat(trackApp.getUsername()).isEqualTo("Michal");
     }
+
+    @Transactional
+    @Test
+    void  shouldUpdateTrack() throws Exception {
+        //given
+        TrackApp newTrack = new TrackApp();
+        newTrack.setName("name");
+        newTrack.setBody("body");
+        newTrack.setUsername("Michal");
+        trackRepo.save(newTrack);
+
+        TrackApp updateTrack = new TrackApp();
+        updateTrack.setName("updateName");
+        updateTrack.setBody("updateName");
+        updateTrack.setUsername("Michal");
+        updateTrack.setId(newTrack.getId());
+
+        //when
+        MvcResult mvcResult = mockMvc.perform(put("/track/update/" + newTrack.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.IF_MATCH, 3)
+                .content(objectMapper.writeValueAsString(updateTrack)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+        //then
+
+    }
+
+
 
 
 }
