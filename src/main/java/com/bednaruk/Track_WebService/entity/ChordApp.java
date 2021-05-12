243e87e5c16a4cb9550858c1fd9e329e2e0207f4
Michał <mbednaruk12@gmail.com>
2021-05-12 10:10:07 +0200

    add builder functionality

--- src/main/java/com/bednaruk/Track_WebService/entity/ChordApp.java
+++ src/main/java/com/bednaruk/Track_WebService/entity/ChordApp.java
@@ -23,6 +23,42 @@ public class ChordApp {
     @NonNull
     private String imageResources;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     private List<TrackApp> trackApps;
+
+    public static Builder builder() {
+        return new Builder();
+    }
+    public static final class Builder {
+        private Long id;
+        private String chordName;
+        private String imageResources;
+        private List<TrackApp> trackApps;
+
+
+        public Builder chordName(Long id) {
+            this.id = id;
+            return this;
+        }
+        public Builder chordName(String chordName) {
+            this.chordName = chordName;
+            return this;
+        }
+        public Builder imageResources(String imageResources) {
+            this.imageResources = imageResources;
+            return this;
+        }
+        public Builder trackApps(List<TrackApp> trackApps) {
+            this.trackApps = trackApps;
+            return this;
+        }
+        public ChordApp build() {
+            ChordApp chordApp = new ChordApp();
+            chordApp.id = this.id;
+            chordApp.chordName = this.chordName;
+            chordApp.imageResources = this.imageResources;
+            chordApp.trackApps = this.trackApps;
+            return chordApp;
+        }
+    }
 }
--- src/main/java/com/bednaruk/Track_WebService/entity/TrackApp.java
+++ src/main/java/com/bednaruk/Track_WebService/entity/TrackApp.java
@@ -1,6 +1,7 @@
 package com.bednaruk.Track_WebService.entity;
 
 import lombok.*;
+import org.atmosphere.interceptor.AtmosphereResourceStateRecovery;
 
 import javax.persistence.*;
 import java.util.List;
@@ -29,4 +30,46 @@ public class TrackApp {
 
     @ManyToMany(mappedBy = "trackApps")
     private List<ChordApp> chordApps;
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private Long id;
+        private String username;
+        private String name;
+        private String body;
+        private List<ChordApp> chordApps;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+        public Builder body(String body) {
+            this.body = body;
+            return this;
+        }
+        public Builder chordApps(List<ChordApp> chordApps) {
+            this.chordApps = chordApps;
+            return this;
+        }
+        public TrackApp build() {
+            TrackApp trackApp = new TrackApp();
+            trackApp.id = this.id;
+            trackApp.name = this.name;
+            trackApp.username = this.username;
+            trackApp.body = this.body;
+            trackApp.chordApps = this.chordApps;
+            return trackApp;
+        }
+    }
 }
