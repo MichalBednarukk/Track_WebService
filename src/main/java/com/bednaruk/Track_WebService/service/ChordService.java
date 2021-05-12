e85c6ba73f7415b39d6aaeb3dbf3bd6c8f94351e
Michał <mbednaruk12@gmail.com>
2021-05-12 11:30:45 +0200

    getAllChord and getSingleChord

--- src/main/java/com/bednaruk/Track_WebService/service/ChordService.java
+++ src/main/java/com/bednaruk/Track_WebService/service/ChordService.java
@@ -5,8 +5,12 @@ import com.bednaruk.Track_WebService.repository.ChordRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 
+@Service
 public class ChordService {
     ChordRepo chordRepo;
 
@@ -15,12 +19,25 @@ public class ChordService {
         this.chordRepo = chordRepo;
     }
 
-    public ResponseEntity<?> addChord(ChordApp chordApp) {
+    public ResponseEntity<Object>  addChord(ChordApp chordApp) {
 
         if(chordApp.getChordName().isEmpty() || chordApp.getImageResources().isEmpty()){
             return ResponseEntity.badRequest().build();
         }
         chordRepo.save(chordApp);
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    public ResponseEntity<Object> getAllChord() {
+        List<ChordApp> chordApps = chordRepo.findAll();
+        return ResponseEntity.ok(chordApps);
+    }
+
+    public ResponseEntity<Object> getSingleChord(String id) {
+        Optional<ChordApp> chordApp = chordRepo.findById(Long.valueOf(id));
+        if(chordApp.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(chordApp.get());
     }
 }
