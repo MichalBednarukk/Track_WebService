3ec2777a3b5a188e0d3e9e9e14e6760523b3ee39
Michał <mbednaruk12@gmail.com>
2021-05-12 11:29:24 +0200

    GetMapping getChordById

--- src/main/java/com/bednaruk/Track_WebService/controller/ChordController.java
+++ src/main/java/com/bednaruk/Track_WebService/controller/ChordController.java
@@ -1,29 +1,32 @@
 package com.bednaruk.Track_WebService.controller;
 
 import com.bednaruk.Track_WebService.entity.ChordApp;
-import com.bednaruk.Track_WebService.entity.TrackApp;
-import com.bednaruk.Track_WebService.repository.ChordRepo;
 import com.bednaruk.Track_WebService.service.ChordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/track")
+@RequestMapping("/chord")
 public class ChordController {
     ChordService chordService;
-    ChordRepo chordRepo;
 
     @Autowired
-    public ChordController(ChordService chordService, ChordRepo chordRepo) {
+    public ChordController(ChordService chordService) {
         this.chordService = chordService;
-        this.chordRepo = chordRepo;
     }
     @PostMapping("/add")
-    public ResponseEntity<?> addChord(@RequestBody ChordApp chordApp){
+    public ResponseEntity<Object> addChord(@RequestBody ChordApp chordApp){
         return chordService.addChord(chordApp);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Object> getAllChord(){
+        return chordService.getAllChord();
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Object> getSingleChord(@PathVariable("id") String id) {
+        return chordService.getSingleChord(id);
     }
 }
